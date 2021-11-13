65e26757951f15b06092662cf381f97b92e951d6
Carl <carl.scrivener.89@gmail.com>
2021-11-13 18:24:23 +0000

    adjust for new how we work api with service items

--- components/how-we-work.js
+++ components/how-we-work.js
@@ -6,15 +6,41 @@ export default function HowWeWork({ items }) {
       {items && (
         <section id="how-we-work">
           <div className="container mx-auto max-w-6xl px-5">
-            {items.map((item) => (
-              <div key={item.id} className="sec-title text-center">
-                <h2 className="wow animated bounceInLeft">{item.title}</h2>
-                <div className="wow animated bounceInRight">
-                  {parse(item.excerpt)}
+            {items.map((item) => {
+              const { serviceItems } = item.serviceItems;
+              return (
+                <div key={item.id} className="sec-title text-center">
+                  <h2 className="wow animated bounceInLeft">{item.title}</h2>
+                  <div className="wow animated bounceInRight">
+                    {parse(item.excerpt)}
+                  </div>
+                  {item.content && (
+                    <div className="py-10">{parse(item.content)}</div>
+                  )}
+                  {serviceItems && (
+                    <div className="grid grid-cols-12 py-14">
+                      {serviceItems.map((serviceItem) => (
+                        <div
+                          key={serviceItem.id}
+                          className="col-span-12 sm:col-span-6 lg:col-span-3 px-5 text-center wow animated zoomIn"
+                        >
+                          <div className="service-item">
+                            <div className="service-icon">
+                              <img
+                                src={serviceItem.featuredImage.node.sourceUrl}
+                                alt={serviceItem.featuredImage.node.altText}
+                              />
+                            </div>
+                            <h3 className="py-4">{serviceItem.title}</h3>
+                            {parse(serviceItem.excerpt)}
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
-                <div className="py-10">{parse(item.content)}</div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </section>
       )}
